d008f1b4f01825f4bb24b7f24fab11fa05bb7b98
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-08-08 22:20:33 +0545

    Created student_list.html template and added delete button to delete from the list

--- srs_app/urls.py
+++ srs_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from srs_app import views
+
 urlpatterns = [
-    
+    path("", views.student_list, name="student-list"),
+    path("delete/<int:pk>/", views.delete_student, name="delete-student"),
 ]
--- srs_app/views.py
+++ srs_app/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from srs_app.models import Student
 
 # Create your views here.
+
+
+def student_list(request):
+    # Fetch all students from the database
+    students = Student.objects.all()
+    return render(
+        request,
+        "student_list.html",
+        {"students": students},
+    )
+
+
+def delete_student(request, pk):
+    # Fetch the student to be deleted
+    student = Student.objects.get(pk=pk)
+    student.delete()
+    return HttpResponseRedirect("/")
